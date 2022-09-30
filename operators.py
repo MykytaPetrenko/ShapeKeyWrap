6bfd6cbea438ac28b65ea7b3ad409e1fafc38575
Mykyta Petrenko <mykyta.petrenko@gmail.com>
2022-09-30 13:33:10 +0300

    replacing target shape keys if the operator executed a couble of times

--- operators.py
+++ operators.py
@@ -20,25 +20,29 @@ def skw_transfer_shapekeys(self, context):
         self.report({'ERROR'}, f'Source (active) object "{obj.name}" does not have shape keys.')
         return
 
-    show_only_shape_key = active.show_only_shape_key
-    active_shape_key_index = active.active_shape_key_index
+    active_obj_show_only_shape_key = active.show_only_shape_key
+    active_obj_active_shape_key_index = active.active_shape_key_index
     active.active_shape_key_index = 0
     active.show_only_shape_key = True
-
     
     num_sk = len(active.data.shape_keys.key_blocks)
-    for obj in selected:
-        if obj is active:
+    for target_obj in selected:
+        if target_obj is active:
             continue
 
-        deformer = obj.modifiers.new(name='surface defrom', type='SURFACE_DEFORM')
+        target_obj_show_only_shape_key = target_obj.show_only_shape_key
+        target_obj_active_shape_key_index = target_obj.active_shape_key_index
+        target_obj.active_shape_key_index = 0
+        target_obj.show_only_shape_key = True
+
+        deformer = target_obj.modifiers.new(name='surface defrom', type='SURFACE_DEFORM')
         deformer.target = active
         bpy.ops.object.modifier_move_to_index(
-            {"object" : obj},
+            {"object" : target_obj},
             modifier=deformer.name, index=0
         )
         bpy.ops.object.surfacedeform_bind(
-            {"object" : obj},
+            {"object" : target_obj},
             modifier=deformer.name
         )
         
@@ -46,16 +50,23 @@ def skw_transfer_shapekeys(self, context):
             active.active_shape_key_index = i
             sk = active.active_shape_key
             deformer.name = sk.name
+            if sk.name in target_obj.data.shape_keys.key_blocks.keys():
+                sk_index = target_obj.data.shape_keys.key_blocks.keys().index(sk.name)
+                target_obj.active_shape_key_index = sk_index
+                bpy.ops.object.shape_key_remove({"object" : target_obj})
             bpy.ops.object.modifier_apply_as_shapekey(
-                {"object" : obj},
+                {"object" : target_obj},
                 keep_modifier=True,
                 modifier=deformer.name, report=False
             )
         active.active_shape_key_index = 0
-        obj.modifiers.remove(deformer)
 
-    active.active_shape_key_index = active_shape_key_index
-    active.show_only_shape_key = show_only_shape_key
+        target_obj.active_shape_key_index = target_obj_active_shape_key_index
+        target_obj.show_only_shape_key = target_obj_show_only_shape_key
+        target_obj.modifiers.remove(deformer)
+
+    active.active_shape_key_index = active_obj_active_shape_key_index
+    active.show_only_shape_key = active_obj_show_only_shape_key
 
 
 class SKW_OT_transfer_shape_keys(bpy.types.Operator):
