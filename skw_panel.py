b5b4baaf5ad9b25a88be60c30a97b7f94952a3f5
Mykyta Petrenko <mykyta.petrenko@gmail.com>
2022-11-27 18:38:32 +0200

    Merging fix

--- skw_panel.py
+++ skw_panel.py
@@ -1,8 +1,8 @@
 import bpy
-from .skw_operators import SKW_OT_transfer_shape_keys
+from .skw_operators import SKW_OT_transfer_shape_keys, skw_poll_transfer_shapekeys
 
 
-class SKW_PT_object_mode(bpy.types.Panel):
+class SKT_PT_object_mode(bpy.types.Panel):
     """
     Addon main menu (N-Panel)
     """
@@ -12,34 +12,27 @@ class SKW_PT_object_mode(bpy.types.Panel):
     bl_category = 'Tools'
     bl_context = 'objectmode'
 
-    def draw(self, context):
+    def draw(self, context):                
         layout = self.layout
-        
-        selected = context.selected_objects
-        active = context.active_object
-        transfer_targets = list()
-        for obj in selected:
-            if obj is active:
-                continue
-            transfer_targets.append(obj)
 
+        result, reason = skw_poll_transfer_shapekeys(context)
+        if result:
+            col = layout.column(align=True)
+            lines = reason.split('\n')
+            for line in lines:
+                col.label(text=line)
+        else:
+            layout.label(text=reason)
         row = layout.row()
         row.operator(SKW_OT_transfer_shape_keys.bl_idname, text='Transfer from Active', icon='ARROW_LEFTRIGHT')
-        
-        if active:
-            layout.row().label(text=f'From: {active.name}')
-        col = layout.column(align=True)
-        if transfer_targets:
-            col.label(text='To:')
-        for obj in transfer_targets:
-            col.label(text=obj.name)
+
 
 def register():
-    bpy.utils.register_class(SKW_PT_object_mode)  
+    bpy.utils.register_class(SKT_PT_object_mode)  
 
 
 def unregister():
-    bpy.utils.unregister_class(SKW_PT_object_mode)
+    bpy.utils.unregister_class(SKT_PT_object_mode)
 
 
 if __name__ == '__main__':
