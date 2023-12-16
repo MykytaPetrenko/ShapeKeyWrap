6811c506831068a4437c69c63088424e2dc75c87
Mykyta Petrenko <mykyta.petrenko@gmail.com>
2023-12-16 11:41:18 +0200

    version 3.3

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ bl_info = {
     'author' : 'Mykyta Petrenko (Squeezy Pixels)',
     'description' : 'Blender addon for transfering shapekeys from on mesh to another one using surface deform modifier.',
     'blender' : (3, 00, 0),
-    'version' : (0, 3, 2),
+    'version' : (0, 3, 3),
     'location' : 'View3D > Object Mode > N-Panel > Tools > Shape Key Wrap',
     'warning' : '',
     'doc_url': 'https://github.com/MykytaPetrenko/ShapeKeyWrap',
